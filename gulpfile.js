63120b71e1099381331b149745aed029c2e4e93d
Patrick Gazaneo <pck1993@gmail.com>
2017-06-18 02:58:18 -0300

    functions

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('scss/*.scss')
+    gulp.src('scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('css'))
 });
 
 gulp.task('default', ['sass'], function() {
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('scss/**/*.scss', ['sass']);
 })
